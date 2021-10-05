b142c14013d5da058af7be56b190de647d76c8d6
woshishuaige2 <56005226+woshishuaige2@users.noreply.github.com>
2021-10-05 12:35:06 -0500

    file downloader

---
+++ app/src/main/java/com/example/filedownloader/ExampleRunnable.java
@@ -0,0 +1,13 @@
+package com.example.filedownloader;
+
+public class ExampleRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        mockFileDownloader();
+    }
+
+    private void mockFileDownloader() {
+    }
+
+}
---
+++ app/src/main/java/com/example/filedownloader/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.filedownloader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private Button startButton;
+    private volatile boolean stopThread = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.button);
+    }
+
+    public void mockFileDownloader(){
+        runOnUiThread(new Runnable(){
+            @Override
+            public void run(){
+                startButton.setText("Downloading");
+            }
+        });
+
+        for (int downloadProgress = 0; downloadProgress <= 100; downloadProgress = downloadProgress+10){
+            if (stopThread){
+                runOnUiThread(new Runnable(){
+                    @Override
+                    public void run(){
+                        startButton.setText("Start");
+                    }
+                });
+                return;
+            }
+
+            Log.d(TAG, "Download Progress: " + downloadProgress + "%");
+            try{
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e ){
+                e.printStackTrace();
+            }
+
+
+        }
+
+        runOnUiThread(new Runnable(){
+            @Override
+            public void run(){
+                startButton.setText("Start");
+            }
+        });
+
+
+    }
+
+    public void startDownload (View view){
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable();
+        new Thread(runnable).start();
+    }
+
+    public void stopThread(View view){
+        stopThread = true;
+    }
+
+
+
+    public class ExampleRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            mockFileDownloader();
+        }
+
+    }
+}
